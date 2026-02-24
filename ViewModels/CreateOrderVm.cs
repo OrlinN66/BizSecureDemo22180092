fb4919718d96bcdc3953586d9c55d18c23487943
Student 22180092 <22180092@unwe.bg>
2026-02-24 16:26:26 +0200

    Ex2: Stored XSS vulnerability - Html.Raw() + MaxLength 300 allows script injection

--- ViewModels/CreateOrderVm.cs
+++ ViewModels/CreateOrderVm.cs
@@ -4,8 +4,8 @@ namespace BizSecureDemo22180092.ViewModels;
 
 public class CreateOrderVm
 {
-    // Ex2: MaxLength reverted back to 80 after XSS demo (was 300 during vulnerability demo)
-    [Required, MaxLength(80)]
+    // VULNERABLE: MaxLength 300 allows long XSS payloads
+    [Required, MaxLength(300)]
     public string Title { get; set; } = "";
 
     [Required]
